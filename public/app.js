cf13c662e0c2e12e24cfde2e36b4d220373a4b1d
ktliu <ktliu@umich.edu>
2016-02-26 03:16:11 -0500

    fixed layout issues and added routing

--- public/app.js
+++ public/app.js
@@ -1,13 +1,67 @@
-var app = angular.module("sampleApp", ["firebase"]);
-
-app.controller("SampleCtrl", function($scope, $firebaseObject) {
+var app = angular.module('koffieApp', ['firebase', 'ui.router']);
  
-  var ref = new Firebase("https://<YOUR-FIREBASE-APP>.firebaseio.com/data");
+
+app.config(function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/home');
+    
+    //https://scotch.io/tutorials/angular-routing-using-ui-router
+    $stateProvider 
+        .state('home', {
+            url: '/home',
+            templateUrl: 'partials/home.html'
+        })
+        .state('login', {
+            url:'/login',
+            templateUrl:'partials/login.html'
+        })
+        .state('profile', {
+            url: '/profile',
+            templateUrl:'partials/profile.html'        
+        })
+        .state('following', {
+            url:'/following',
+            templateUrl:'partials/following.html'
+        })
+        .state('help', {
+            url:'/help',
+            templateUrl:'partials/help.html'
+        });
+});
+
+
+app.controller('mainCtrl', function($scope, $firebaseObject, $firebaseAuth) {
+ 
+   
+  var ref = new Firebase('https://boiling-heat-1276.firebaseio.com');
   // download the data into a local object
-  var syncObject = $firebaseObject(ref);
+  //var syncObject = $firebaseObject(ref);
   // synchronize the object with a three-way data binding
   // click on `index.html` above to see it used in the DOM!
-  syncObject.$bindTo($scope, "data");
+  //syncObject.$bindTo($scope, "data");
+ 
+  function authHandler(error, authData) {
+      if (error) {
+          console.log("Login Failed!", error);
+      } else {
+          console.log("Authenticated successfully with payload:", authData);
+      }
+  }
+   
+  $scope.submitForm = function(isValid) {
+
+    // check to make sure the form is completely valid
+       if (isValid) {
+           alert('our form is amazing');
+           var auth = $firebaseAuth(ref);
+           
+           // login with Facebook
+            ref.authWithPassword({
+              email    : 'bobtony@firebase.com',
+              password : 'correcthorsebatterystaple'
+            }, authHandler);
+       }
+
+   };
 });
 
 
